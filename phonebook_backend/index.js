ead38c1623f65c898056ec2c3b3723f0dd158e13
ilijaepler <eplerilija17@gmail.com>
2022-08-22 16:51:16 +0200

    part3 3.7-3.8

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 let persons = [
@@ -25,6 +26,7 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res[body]'))
 
 app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p>
