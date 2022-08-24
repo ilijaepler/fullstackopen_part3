e7bf1ed12c67a1125d98cec93212066b194f69d9
ilijaepler <eplerilija17@gmail.com>
2022-08-24 16:48:09 +0200

    fixing gitignore

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -93,7 +93,6 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-// test comment
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
