3e10d4bac99274530ad1b7d5c3f912142db57d70
ilijaepler <eplerilija17@gmail.com>
2022-08-24 18:47:47 +0200

    frontend production build more4

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -25,12 +25,7 @@ let persons = [
     }
 ]
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
